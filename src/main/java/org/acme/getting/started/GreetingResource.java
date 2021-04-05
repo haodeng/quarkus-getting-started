9571e6dcf7d3ebc40848945d92613acd5c2ac5a6
hao <hao.deng.m@gmail.com>
2021-04-05 16:17:00 +0200

    Add new test endpoint

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -5,13 +5,22 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-// curl -w "\n" http://localhost:8080/hello
+
 @Path("/hello")
 public class GreetingResource {
 
+    // curl -w "\n" http://localhost:8080/hello
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello Me";
+    }
+
+    // curl -w "\n" http://localhost:8080/hello/json
+    @GET
+    @Path("/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String helloJson() {
+        return "{\"message\": \"Hello Me\"}";
     }
 }
